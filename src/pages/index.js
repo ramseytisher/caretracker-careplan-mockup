ae32eebd769a54b2f172cc55c755c5baca62ed22
rt022748 <ramsey.tisher@cerner.com>
2020-07-17 16:20:49 -0400

    updated nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Header from "../components/header"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 import { Typography, Button } from "antd"
-import { HighlightTwoTone } from "@ant-design/icons"
 
 const { Title } = Typography
 
 export default () => {
   return (
     <Layout>
-      <Title>
-            <Link
-              to="/"
-              style={{
-                textDecoration: `none`,
-                color: "#5C6770",
-              }}
-            >
-              <HighlightTwoTone /> CareTracker Mockups
-            </Link>
-          </Title>
-      <Button size="large">
-        <Link to="/care-plan">Care Plan Mockup</Link>
-      </Button>
+      <Header />
     </Layout>
   )
 }
